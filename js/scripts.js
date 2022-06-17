36a813f90c4c4636e4827c36528f1aa3ba96d421
Alex Shevlin <alexshevlin1@gmail.com>
2022-06-17 11:51:30 -0700

    add how much button to display price from getPizza();

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,23 @@
+// function Sold() {
+//   this.pizzas = {};
+//   this.currentId = 0;
+// }
+
+// Sold.prototype.addPizza = function(pizza) {
+//   pizza.id = this.assignId();
+//   this.pizzas[pizza.id] = pizza;
+// }
+
+// Sold.prototype.assignId = function() {
+//   this.currentId += 1;
+//   return this.currentId;
+// }
 function Pizza(size, sauce, dipSauce) {
   this.size = size;
   this.sauce = sauce;
   this.dipSauce = dipSauce;
-  this.topping = [];
+  this.toppings = [];
+  this.priceMsg = this.finalPrice;
 }
 
 const size = [
@@ -42,32 +57,115 @@ Pizza.prototype.finalPrice = function() {
       finalPrice += dipSauce[i].price;
     }
   }
+  for (let i = 0; i < this.toppings.length; i++) {
+    finalPrice += this.toppings[i].length;
+  }
+  console.log(finalPrice);
   return finalPrice;
 }
+
+// let sold = Sold();
 
 $(document).ready(function() {
   $('#start').click(function(event) {
     event.preventDefault();
     $('.mainPage').fadeOut('fast');
     $('.pizzaForm').fadeIn();
+    console.log('click');
   })
 
-  $("form#pizza").submit(function(event) {
+  $("#howmuch-button").click(function(event) {
     event.preventDefault();
-
-    const size = $('select#sizeList').val();
-    const sauce = $('select#sauceList').val();
-    const dip = $('select#dipList').val();
-    let topping = [];
-
-    $('input:checkbox[name=topping]:checked').each(function() {
-      topping.push($(this).val());
-    });
-
-    let pizza1 = new Pizza(size, sauce, dip);
-    pizza1.topping.push(topping);
-    let cost1 = pizza1.finalPrice();
-
-    $()
+    let newPizza = getPizza();
+    let cost1 = newPizza.finalPrice();
+    $('#pizza-price').text("$ " + cost1);
+    $('#buy-button').removeAttr('disabled');
+  })
+  $('#buy-button').click(function(event) {
+    event.preventDefault();
+    let newPizza = getPizza();
+    attachSoldPizzas(newPizza);
+    $('#buy-button').prop('disabled', true);
+    clearPizzaForm();
   })
 })
+
+function getPizza() {
+  const size = $('select#sizeList').val();
+  const sauce = $('select#sauceList').val();
+  const dipSauce = $('select#dipList').val();
+  let toppings = [];
+  let newPizza = new Pizza(size, sauce, dipSauce);
+  $('input:checkbox[name=topping]:checked').each(function() {
+    toppings.push($(this).val());
+  });
+  newPizza.toppings.push(toppings);
+  return newPizza;
+}
+
+function attachSoldPizzas(pizza) {
+  const size = document.createElement('ul');
+  size.innerHTML = pizza.size;
+  const sauce = document.createElement('li');
+  sauce.innerHTML = pizza.sauce;
+  const toppings = document.createElement('li');
+  toppings.innerHTML = pizza.toppings;
+  const dip = document.createElement('li');
+  dip.innerHTML = pizza.dipSauce;
+  const price = document.createElement('li');
+  price.innerHTML = "$ " + pizza.priceMsg;
+  size.appendChild(sauce);
+  size.appendChild(sauce);
+  size.appendChild(toppings);
+  size.appendChild(dip);
+  $('#pizzas-items-list').append(size);
+  $('.receipt h4').show();
+}
+
+function clearPizzaForm() {
+  $('input:checkbox[name=topping]').prop('checked', false);
+  $('.cart').toggle();
+}
+
+//switch case for finalPrice
+  // for (topping in topping) {
+  //   finalPrice += 1
+  // }
+
+  // switch (size) {
+  //   case("12"):
+  //     finalPrice += 4;
+  //   case("16"):
+  //     finalPrice += 6;
+  //   case("20"):
+  //     finalPrice += 7;
+  //   case("24"):
+  //     finalPrice += 8;
+  //     break;
+  //   default:
+  //     console.log("Not pizza Size")
+  // }
+
+  // switch (sauce) {
+  //   case("Marinara"):
+  //     finalPrice += 0;
+  //   case("Pesto"):
+  //     finalPrice += 1;
+  //   case("Alfredo"):
+  //     finalPrice += 1;
+  //     break;
+  //   default:
+  //     console.log("Not pizza sauce")
+  // }
+
+  // switch (dipSauce) {
+  //   case("Marinara"):
+  //     finalPrice += 0;
+  //   case("Pesto"):
+  //     finalPrice += 1;
+  //   case("Alfredo"):
+  //     finalPrice += 1;
+  //     break;
+  //   default:
+  //     console.log("Not pizza sauce")
+  // }
